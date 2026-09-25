64999d67c9029d8dee955233fc80d5afc2c80386
nvgrigorev <n.v.grigorev@tmn4.etagi.com>
2026-09-25 15:22:12 +0500

    ui: simplify design upgrade pricing labels

--- public/app.js
+++ public/app.js
@@ -286,7 +286,7 @@ function renderDesignProjects() {
       ? '<strong class="design-included-price"><s>' + rub.format(project.pricePerM2) + ' / м²</s></strong><span class="design-gift-label">В подарок</span>'
       : '<strong>+' + rub.format(project.upgradePricePerM2) + ' / м²</strong>' +
         '<span>' + (area > 0
-          ? '≈ ' + rub.format(upgradeTotal) + ' · полный проект ' + rub.format(retailTotal)
+          ? 'Полный проект ' + rub.format(retailTotal)
           : 'Полная стоимость проекта ' + rub.format(project.pricePerM2) + ' / м²') + '</span>';
     const cta = project.included
       ? '<div class="design-included-button">Входит в подарок</div>'
